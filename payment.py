6cb33af1f37db7b98629fba9b806433e24ba40d7
Siomara N Alonso Hernandez <xiomara281190@gmail.com>
2022-04-26 21:11:43 +0000

    Updated payment and added bizum_credentials.txt and paypal_credentials.txt.

--- payment.py
+++ payment.py
@@ -1,5 +1,5 @@
 from os import system, name
-from abc import ABC, abstractmethod
+import json
 
 if name == "nt":
     system('cls')
@@ -7,17 +7,97 @@ else:
     system('clear')
 
 
-class Payment(ABC):
+class Payment:
 
     def __init__(self):
+        self.__operation = {
+            self.__gen_operation_code(): [self.__username, self.__total_cost]
+            }
+    
+
+    def last_key(self):
+        return 1 if len(self.__operation) == 0 else (list(self.__operation.keys())[-1]) + 1
+
+
+    def __gen_operation_code(self):
         pass
 
 
 class Card:
-    pass
+    
+    def __init__(self, name, card_number, expiration_date, cvv):
+        self.__name = name
+        self.__card_number = card_number
+        self.__expiration_date = expiration_date
+        self.__cvv = cvv
+
+
 
 class Bizum:
-    pass
+
+    __BIZUM_CRED_FILE = "bizum_credentials.txt"
+    
+    def __init__(self):
+        self.__send_bizum_gateway()
+
+
+    def file_as_list(self):
+        with open(self.__BIZUM_CRED_FILE, "r") as f:
+            return [json.loads(credentials) for credentials in f.readlines()]
+
+
+    def _check_credentials(self, tel_num, pin):
+        cred_l = self.file_as_list()
+
+        for credentials in cred_l:
+            if tel_num == credentials["phone"] and pin == credentials["pin"]:
+                return True
+
+        return False
+        
+
+    def __send_bizum_gateway(self):
+        
+        tel_num = input("Introduzca su número de teléfono: ")
+        pin = input("Introduzca su PIN de bizum (4 dígitos): ")
+
+        if self._check_credentials(tel_num, pin):
+            return True
+        
+        return False
+
+        
 
 class PayPal:
-    pass
+
+    __PAYPAL_CRED_FILE = "paypal_credentials.txt"
+
+    
+    def __init__(self):
+        self.__send_paypal_gateway()
+        
+
+    def file_as_list(self):
+        with open(self.__PAYPAL_CRED_FILE, "r") as f:
+            return [json.loads(credentials) for credentials in f.readlines()]
+
+
+    def _check_credentials(self, email, password):
+        cred_l = self.file_as_list()
+
+        for credentials in cred_l:
+            if email == credentials["email"] and password == credentials["password"]:
+                return True
+
+        return False
+
+
+    def __send_paypal_gateway(self):
+        
+        email = input("Introduzca su email de paypal: ")
+        password = input("Introduzca su contraseña de paypal: ")
+
+        if self._check_credentials(email, password):
+            return True
+        
+        return False
