d7bdc8dd6f08a2f90086fbd98738fda5a5a122dc
Siomara Alonso <xiomara281190@gmail.com>
2022-04-29 20:53:08 +0100

    Updated everything.

--- modules/modules.py
+++ modules/modules.py
@@ -1,7 +1,4 @@
 from json import *
-# from clear import *
-
-# clear()
 
 
 def file_as_list(filename):
@@ -14,9 +11,27 @@ def last_key(dict):
 
 
 def write_json(new_data, filename):
-    with open(filename,'r+') as f:
-        file_data = load(f)
-        file_data.append(new_data)
-        f.seek(0)
-        dump(file_data, f, indent = 4)
-     
+    file_data = file_as_list(filename)
+
+    with open(filename, 'a') as f:
+        f.write(dumps(new_data) + '\n')
+
+# def user_exists(username, filename):
+#     file_data = file_as_list(filename)
+#     print(file_data)
+    # for data in file:
+    #     print(data)
+
+def anadir_ingrediente(n_recipe, n_ingredient):
+
+    recipe_file = file_to_list("recetas_v1.txt")
+
+    with open("aux_recipe.txt", "w") as f:
+
+        for recipe in recipe_file:
+            if recipe["nombre"] == n_recipe and n_ingredient not in recipe["ingredientes"]:
+                recipe["ingredientes"].append(n_ingredient)
+
+            f.write(json.dumps(recipe) + "\n")
+
+        rename_file("aux_recipe.txt", "recetas_v1.txt")
