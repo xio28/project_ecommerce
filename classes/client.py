19d7028c2e151dc3b482fdfa08f779851d6069dc
Siomara N Alonso Hernandez <xiomara281190@gmail.com>
2022-04-29 23:07:14 +0000

    Updated modules.

--- classes/client.py
+++ classes/client.py
@@ -14,54 +14,44 @@ import json
 
 clear()
 
-class Client:
+# class Client:
 
-    # JSON_FILE = r"C:\xampp\htdocs\project_ecommerce\database\client_info.json"
-    JSON_FILE = "/home/cfgs1/Documentos/repo/project_ecommerce/database/client_info.json"
+#     JSON_FILE = r"C:\xampp\htdocs\project_ecommerce\database\client_info.txt"
+#     # JSON_FILE = "/home/cfgs1/Documentos/repo/project_ecommerce/database/client_info.txt"
 
-    def __init__(self, username, email, password, name, nid, address, contact, recommended, pets = {}, payments = {}):
-        self._username = username
-        self._email = email
-        self.__password = password
-        self._name = name
-        self._nid = nid
-        self._address = address
-        self._contact = contact
-        self._recommended = recommended
-        self._pets = pets
-        self._payments = payments
-        self.user_info()
-        self.load_json()
+#     def __init__(self, username, email, password, name, nid, address, contact, payments = {}):
+#         self._username = username
+#         self._email = email
+#         self.__password = password
+#         self._name = name
+#         self._nid = nid
+#         self._address = address
+#         self._contact = contact
+#         self._payments = payments
+#         self.store_user_info()
 
 
 
-    def user_info(self):
-        with open(self.JSON_FILE, "r+") as f:
-            user_info = {
-                # "_id": last_key(self._user_info),
-                "username": self._username,
-                "email": self._email,
-                "password": self.__password,
-                "name": self._name,
-                "NID": self._nid,
-                "phone": self._contact,
-                "recommended": self._recommended,
-                "address": self._address,
-                "pets": self._pets,
-                "payments": self._payments
-            }
+#     def store_user_info(self):
+#         with open(self.JSON_FILE, "r+") as f:
+#             user_info = {
+#                 # "_id": last_key(self._user_info),
+#                 "username": self._username,
+#                 "email": self._email,
+#                 "password": self.__password,
+#                 "name": self._name,
+#                 "NID": self._nid,
+#                 "phone": self._contact,
+#                 "address": self._address,
+#                 "payments": self._payments
+#             }
 
-            write_json(user_info, self.JSON_FILE)
-
-    
-    def load_json(self):
-        with open(self.JSON_FILE, "r+") as f:
-            print(f.read())
+#             write_json(user_info, self.JSON_FILE, "username", self._username)
 
 
 
-client1 = Client("ilos28", "holaMundo", "xiomara@gmail.com", "Siomara Alonso", "44444444T", {"street": "C/Torres 1, 2º D", "postal_code": "35017", "city": "Las Palmas de G.C."}, ["678678678", "696696696"], True, [{"species": "perro", "pet_name": "Robin", "birthdate": "06/03/2014"}, {"species": "gato", "pet_name": "Deny", "birthdate": "20/09/2009"}])
-client2 = Client("carcoal", "holaMundo", "chris@gmail.com", "Chris Medina", "33333333T", {"street": "C/Vega 1, 3º D", "postal_code": "35022", "city": "Telde"}, ["645124753"], False, [{"species": "gato", "pet_name": "Vilma", "birthdate": "28/10/2020"}])
+# client1 = Client("ilos28", "holaMundo", "xiomara@gmail.com", "Siomara Alonso", "44444444T", {"street": "C/Torres 1, 2º D", "postal_code": "35017", "city": "Las Palmas de G.C."}, ["678678678", "696696696"])
+# client2 = Client("carcoal", "holaMundo", "chris@gmail.com", "Chris Medina", "33333333T", {"street": "C/Vega 1, 3º D", "postal_code": "35022", "city": "Telde"}, ["645124753"])
 
 
 # client1.user_info()
--- modules/modules.py
+++ modules/modules.py
@@ -1,4 +1,5 @@
 from json import *
+from os import stat
 # from clear import *
 
 # clear()
@@ -13,10 +14,34 @@ def last_key(dict):
     return 1 if len(dict) == 0 else (list(dict)[-1]) + 1
 
 
-def write_json(new_data, filename):
-    with open(filename,'r+') as f:
-        file_data = load(f)
-        file_data.append(new_data)
-        f.seek(0)
-        dump(file_data, f, indent = 4)
-     
+def write_json(new_data, filename, key, condition):
+    try:
+        with open(filename, 'a') as f:
+            if check_key_condition(key, condition, filename):
+                print(f"El {key} {condition} ya existe.")
+                return False
+            else:
+                f.write(dumps(new_data) + '\n')
+
+    except IOError:
+        with open(filename, 'w') as f:
+            f.write(dumps(new_data) + '\n')
+
+
+def check_if_file_empty(filename):
+    if stat(filename) == 0:
+        return True
+    else:
+        return False
+    
+
+# Comprueba si una condición (nombre, número, etc.) está dentro del diccionario
+def check_key_condition(key, condition, filename):
+    file_data = file_as_list(filename)
+
+    with open(filename, 'r') as f:
+        for data in file_data:
+            if data[key] == condition:
+                return True
+        
+        return False
