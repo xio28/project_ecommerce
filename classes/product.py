250870e319cfb7210dbaa122c3dbc8e8cb3aa4cc
Siomara N Alonso Hernandez <xiomara281190@gmail.com>
2022-05-08 21:06:59 +0000

    Updated product.py and products.txt.

--- classes/product.py
+++ classes/product.py
@@ -22,7 +22,7 @@ class Products:
 
     def add_products_to_file(self, obj):
         try:
-            if not check_id(id, PRODUCTS):
+            if check_id(id, PRODUCTS):
                 write_json(obj, PRODUCTS)
 
             else:
@@ -47,17 +47,17 @@ class Products:
 
 
 p = Products()
-p1 = Products(1, "Nature's Variety Original Medium Adult pollo", "perro", "pienso")
-p2 = Products(2, "Salvaje Base Esterilizado pienso", "gato", "pienso")
-p3 = Products(3, "Alpha Spirit Albóndigas de Ciervo con Romero", "perro", "comida húmeda")
-p4 = Products(4, "Wellness Core Tender Cuts Chicken Selection", "gato", "comida húmeda")
-p5 = Products(5, "Nayeco X-TRM Cronos Neón Limón arnés", "perro", "arneses")
-p6 = Products(6, "Sepicat Light Weight Extreme Fresh arena aglomerante ligera", "gato", "arena")
+p1 = Products(1, "Nature's Variety Original Medium Adult pollo 12kg", "perro", "pienso", 59.95)
+p2 = Products(2, "Salvaje Base Esterilizado pienso 12kg", "gato", "pienso", 24.95)
+p3 = Products(3, "Alpha Spirit Albóndigas de Ciervo con Romero", "perro", "comida húmeda", 2.99)
+p4 = Products(4, "Wellness Core Tender Cuts Chicken Selection", "gato", "comida húmeda", 6.39)
+p5 = Products(5, "Nayeco X-TRM Cronos Neón Limón arnés talla L", "perro", "arneses", 15.29)
+p6 = Products(6, "Sepicat Light Weight Extreme Fresh arena aglomerante ligera 16L", "gato", "arena", 14.99)
 
-# p.add_products_to_file(p1)
-# p.add_products_to_file(p2)
-# p.add_products_to_file(p3)
-# p.add_products_to_file(p4)
-# p.add_products_to_file(p5)
-# p.add_products_to_file(p6)
-p.return_products()
+p.add_products_to_file(p1)
+p.add_products_to_file(p2)
+p.add_products_to_file(p3)
+p.add_products_to_file(p4)
+p.add_products_to_file(p5)
+p.add_products_to_file(p6)
+# p.return_products()
