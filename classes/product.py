91ce05df72edb08cddbef4ccf3243c4308c2a0df
Siomara N Alonso Hernandez <xiomara281190@gmail.com>
2022-05-08 21:09:35 +0000

    Updated project.

--- classes/product.py
+++ classes/product.py
@@ -38,7 +38,7 @@ class Products:
         result = []
 
         for product in products_file:
-            result.append(product["p_name"])
+            result.extend([product["p_name"], product['price']])
         
         return result
 
@@ -54,10 +54,10 @@ p4 = Products(4, "Wellness Core Tender Cuts Chicken Selection", "gato", "comida 
 p5 = Products(5, "Nayeco X-TRM Cronos Neón Limón arnés talla L", "perro", "arneses", 15.29)
 p6 = Products(6, "Sepicat Light Weight Extreme Fresh arena aglomerante ligera 16L", "gato", "arena", 14.99)
 
-p.add_products_to_file(p1)
-p.add_products_to_file(p2)
-p.add_products_to_file(p3)
-p.add_products_to_file(p4)
-p.add_products_to_file(p5)
-p.add_products_to_file(p6)
-# p.return_products()
+# p.add_products_to_file(p1)
+# p.add_products_to_file(p2)
+# p.add_products_to_file(p3)
+# p.add_products_to_file(p4)
+# p.add_products_to_file(p5)
+# p.add_products_to_file(p6)
+print(p.return_products())
